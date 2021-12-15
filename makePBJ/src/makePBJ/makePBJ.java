7a6ca4843ab297cc942376863df71835df6f2a40
corey <corey@192.168.254.23>
2021-12-15 05:04:31 -0500

    update

--- makePBJ/src/makePBJ/makePBJ.java
+++ makePBJ/src/makePBJ/makePBJ.java
@@ -12,6 +12,32 @@ public class makePBJ {
 	/**
 	 * @param args
 	 */
+	
+	Scanner input = new Scanner(System.in);
+	
+	public static void makeSandwich(int totalInt) {
+
+	
+		
+		System.out.println("step 1: gather ingredients");
+		System.out.println("        " + totalInt + " oz(s) of peanut butter");
+		System.out.println("        " + totalInt + " oz(s) of jelly");
+		System.out.println("        " + 2 * totalInt + " slice(s) of bread");
+		System.out.println("        "+ totalInt + " ziplock bag(s)");
+		System.out.println(" ");
+		System.out.println("step 2: spread " + totalInt + " oz(s) of peanut butter on " + totalInt + " slice(s) of bread");
+		System.out.println(" ");
+		System.out.println("step 3: spread " + totalInt + " oz(s) of jelly on " + totalInt + " slice(s) of bread");
+		System.out.println(" ");
+		System.out.println("step 4: combine " + totalInt + " peanut butter slice(s) with " +totalInt + " jelly splice(s)");
+		System.out.println(" ");
+		System.out.println("step 5: place " + totalInt + " sandwich(s) on " + totalInt + " ziplock bag(s)");
+		System.out.println(" ");
+		System.out.println("step 6: pack " + totalInt + " sandwich(s) for picnic");
+		System.out.println(" ");
+		System.out.println("step 7: unpack and enjoy!");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -19,7 +45,7 @@ public class makePBJ {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println(" ");
-		System.out.println("************************************");
+		System.out.println("************************************************************************");
 		
 		System.out.println("how many sandwiches do you want?");
 		int userInt = input.nextInt();
@@ -32,9 +58,13 @@ public class makePBJ {
 		int totalInt = userInt + myInt;
 		
 		
-		System.out.println("i will take " + myInt + " sandwiches.");
-		System.out.println("************************************");
-		System.out.println("so we should pack " + totalInt + " pb&j sandwiches.");
+		System.out.println(" ");
+		System.out.println("i will eat " + myInt + " sandwiches");
+		System.out.println("************************************************************************");
+		System.out.println("let's make " + totalInt + " pb&j sandwiches");
+		System.out.println(" ");
+		
+		makeSandwich(totalInt);
 	}
 
 }
